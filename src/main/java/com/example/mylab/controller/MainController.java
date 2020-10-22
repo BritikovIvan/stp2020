6bd3f743bb16ec8299320f02659ab96f6db22156
Ivan <britikovivan@gmail.com>
2020-10-23 02:31:09 +0300

    one-to-many

--- src/main/java/com/example/mylab/controller/MainController.java
+++ src/main/java/com/example/mylab/controller/MainController.java
@@ -1,14 +1,15 @@
 package com.example.mylab.controller;
 
 import com.example.mylab.domain.Message;
+import com.example.mylab.domain.User;
 import com.example.mylab.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -33,9 +34,9 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+    public String add(@AuthenticationPrincipal User user, @RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
 
-        Message message = new Message(text, tag);
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
