c555537cf279b951d1ecc5b8fbccde7d59b6a84f
Ivan <ivanbritikov@gmail.com>
2020-11-22 03:28:24 +0300

    reCaptcha-rememberMe

--- src/main/java/com/example/mylab/service/UserService.java
+++ src/main/java/com/example/mylab/service/UserService.java
@@ -114,7 +114,7 @@ public class UserService implements UserDetailsService {
         }
 
         if (!StringUtils.isEmpty(password)) {
-            user.setPassword(password);
+            user.setPassword(passwordEncoder.encode(password));
         }
 
         userRepo.save(user);
